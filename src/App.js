cd57a58c5e14a00984467168d2d6bfe66b578a1d
nguyentien <nguyentien@deakin.edu.au>
2023-03-03 12:43:01 +1100

    add responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./Routes/Home";
-import CoinPage from "./Routes/CoinPage";
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  MenuItem,
+  Select,
+} from "@mui/material";
 
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
 import classes from "./Header.module.css";
 import { CryptoState } from "../CrytoContext";
 
@@ -10,7 +15,7 @@ const Header = () => {
   const { currency, setCurrency } = CryptoState();
 
   return (
-    <AppBar color="transparent" position="static">
+    <AppBar color="transparent" position="sticky">
       <Container>
         <Toolbar>
           <Typography className={classes.title} variant="h6">
@@ -23,12 +28,18 @@ const Header = () => {
               width: 100,
               height: 40,
               marginRight: 15,
+              paddingBottom: 15,
+              overflow: "hidden",
             }}
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
           >
-            <MenuItem className={classes.items} value={"USD"}>USD</MenuItem>
-            <MenuItem className={classes.items} value={"AUD"}>AUD</MenuItem>
+            <MenuItem className={classes.items} value={"USD"}>
+              USD
+            </MenuItem>
+            <MenuItem className={classes.items} value={"AUD"}>
+              AUD
+            </MenuItem>
           </Select>
         </Toolbar>
       </Container>
